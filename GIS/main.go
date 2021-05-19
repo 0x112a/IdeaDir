3716e37678933caf8d0bea480f878bd0bff2e878
0x112a <1436063244@qq.com>
2021-05-20 06:15:48 +0800

    The archive,Next rebuild

--- GIS/main.go
+++ GIS/main.go
@@ -2,19 +2,14 @@ package main
 
 import (
 	mysql_driver "GIS/mysql-driver"
-	"GIS/openfile"
-	"bufio"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"strconv"
-	"strings"
 	"time"
 )
 
 func main()  {
-	dsn := "root@tcp(127.0.0.1:3306)/GIS"
+	dsn := "root:1q2w3e@tcp(47.56.103.207:3306)/GIS"
 	//filepath := "/home/monica/IdeaProjects/GIS/gps_20161101"
-	filepath := "/root/gps_20161101"
+	//filepath := "/root/gps_20161101"
 	db := mysql_driver.Connect(dsn)
 	err := db.Ping()
 	checkErr(err)
@@ -22,37 +17,37 @@ func main()  {
 	defer db.Close()
 
 
-	f := openfile.Read(filepath)
-	defer f.Close()
-
-
-	stmt ,err := db.Prepare("INSERT INTO track SET carID=?,indentID=?,t=?,lon=?,lat=?")
-	checkErr(err)
-
-	begin := time.Now()
-	input := bufio.NewScanner(f)
-	car ,index := "",0
-	indent ,index1 := "",0
-	i:=0
-	for input.Scan(){
-		temp := strings.Split(input.Text(),",")
-		fmt.Println(i," | ",temp)
-		if car != temp[0]{
-			car = temp[0]
-			index++
-		}
-		if indent != temp[1]{
-			indent = temp[1]
-			index1++
-		}
-		index2,_:= strconv.Atoi(temp[2])
-		//checkErr(err)
-		//mysql_driver.Insert(db,int64(index),int64(index1),int64(index2),temp[3],temp[4])
-		mysql_driver.Insert(stmt,int64(index),int64(index1),int64(index2),temp[3],temp[4])
-		i++
-	}
-	end := time.Since(begin)
-	fmt.Println(end.Seconds(),"\tCount:",i)
+	//f := openfile.Read(filepath)
+	//defer f.Close()
+	//
+	//
+	//stmt ,err := db.Prepare("INSERT INTO track SET carID=?,indentID=?,t=?,lon=?,lat=?")
+	//checkErr(err)
+	//
+	//begin := time.Now()
+	//input := bufio.NewScanner(f)
+	//car ,index := "",0
+	//indent ,index1 := "",0
+	//i:=0
+	//for input.Scan(){
+	//	temp := strings.Split(input.Text(),",")
+	//	fmt.Println(i," | ",temp)
+	//	if car != temp[0]{
+	//		car = temp[0]
+	//		index++
+	//	}
+	//	if indent != temp[1]{
+	//		indent = temp[1]
+	//		index1++
+	//	}
+	//	index2,_:= strconv.Atoi(temp[2])
+	//	//checkErr(err)
+	//	//mysql_driver.Insert(db,int64(index),int64(index1),int64(index2),temp[3],temp[4])
+	//	mysql_driver.Insert(stmt,int64(index),int64(index1),int64(index2),temp[3],temp[4])
+	//	i++
+	//}
+	//end := time.Since(begin)
+	//fmt.Println(end.Seconds(),"\tCount:",i)
 	//fmt.Println(end.Seconds())
 }
 
