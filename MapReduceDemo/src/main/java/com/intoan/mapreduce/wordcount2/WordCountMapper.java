56813a6cd473fe765a3ffd576294842cc57defc4
0x112a <1436063244@qq.com>
2021-07-26 22:00:22 +0800

    add wordCount2

---
+++ MapReduceDemo/src/main/java/com/intoan/mapreduce/wordcount2/WordCountMapper.java
@@ -0,0 +1,37 @@
+package com.intoan.mapreduce.wordcount2;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * KEYIN, map阶段输入的key的类型 LongWritable
+ * VALUEIN,...Text
+ * KEYOUT，... Text
+ * VALUEOUT, ...IntWritable
+ */
+public class WordCountMapper extends Mapper<LongWritable, Text,Text,IntWritable> {
+
+    private Text outText = new Text();
+
+    private IntWritable outValue = new IntWritable(1);
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        String line = value.toString();
+
+        String[] words = line.split(" ");
+
+        for (String word : words) {
+
+            outText.set(word);
+
+            context.write(outText,outValue);
+        }
+
+    }
+}
---
+++ MapReduceDemo/src/main/java/com/intoan/mapreduce/wordcount2/WordCountReduce.java
@@ -0,0 +1,30 @@
+package com.intoan.mapreduce.wordcount2;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * KEYIN, reduce阶段输入的key的类型 Text
+ * VALUEIN,... IntWritable
+ * KEYOUT，... 输出...Text
+ * VALUEOUT, ...IntWritable
+ */
+public class WordCountReduce extends Reducer<Text, IntWritable, Text,IntWritable> {
+
+    private IntWritable outValue = new IntWritable();
+
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+        int sum = 0;
+        for (IntWritable value : values) {
+            sum += value.get();
+        }
+
+        outValue.set(sum);
+        context.write(key,outValue);
+    }
+}
